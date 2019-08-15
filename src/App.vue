4145494ab2d1cc8205e531f110473d477d1b7b5c
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-08-15 09:36:08 +0800

    Added NavBar component into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <NavBar />
+    <!-- <router-view /> -->
   </div>
 </template>
 
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    NavBar
+  }
 };
 </script>
 
