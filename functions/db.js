58148a0ca5e3a8fc5b7ea193d74241a385c68bb1
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-08-19 14:24:14 +0800

    Created a new "db" module for the cloud functions:     Scaffolded module that will be holding all the Database action wrapper     functions for other modules and the cloud functions to execute database     actions in the same consistent way, and to have it only implemented once.

---
+++ functions/db.js
@@ -0,0 +1,15 @@
+/*  @Doc
+    Module holding all the wrapper functions for the different database operations.
+*/
+
+const functions = require('firebase-functions');
+
+
+// Function to save a email into the Database
+function saveEmail(email) {
+
+}
+
+module.exports = {
+    saveEmail
+};
