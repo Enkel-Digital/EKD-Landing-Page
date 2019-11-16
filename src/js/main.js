254d21f2e6c14b4352831a1ce7bfeae3f09d0856
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-11-16 15:55:12 +0800

    Added npm "lint" script and ran it. 1.  Added new npm run script, "lint:js" to run linter. 2.  Ran the linter fixing all the issues in the JS files.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 AOS.init({
 	duration: 800,
-	easing: 'slide',
+	easing: "slide",
 	once: true
 });
 
@@ -9,28 +9,28 @@ jQuery(document).ready(function ($) {
 
 	function siteMenuClone() {
 
-		$('.js-clone-nav').each(function () {
+		$(".js-clone-nav").each(function () {
 			var $this = $(this);
-			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+			$this.clone().attr("class", "site-nav-wrap").appendTo(".site-mobile-menu-body");
 		});
 
 
 		setTimeout(function () {
 
 			var counter = 0;
-			$('.site-mobile-menu .has-children').each(function () {
+			$(".site-mobile-menu .has-children").each(function () {
 				var $this = $(this);
 
-				$this.prepend('<span class="arrow-collapse collapsed">');
+				$this.prepend("<span class=\"arrow-collapse collapsed\">");
 
-				$this.find('.arrow-collapse').attr({
-					'data-toggle': 'collapse',
-					'data-target': '#collapseItem' + counter,
+				$this.find(".arrow-collapse").attr({
+					"data-toggle": "collapse",
+					"data-target": "#collapseItem" + counter,
 				});
 
-				$this.find('> ul').attr({
-					'class': 'collapse',
-					'id': 'collapseItem' + counter,
+				$this.find("> ul").attr({
+					"class": "collapse",
+					"id": "collapseItem" + counter,
 				});
 
 				counter++;
@@ -39,12 +39,12 @@ jQuery(document).ready(function ($) {
 
 		}, 1000);
 
-		$('body').on('click', '.arrow-collapse', function (e) {
+		$("body").on("click", ".arrow-collapse", function (e) {
 			var $this = $(this);
-			if ($this.closest('li').find('.collapse').hasClass('show')) {
-				$this.removeClass('active');
+			if ($this.closest("li").find(".collapse").hasClass("show")) {
+				$this.removeClass("active");
 			} else {
-				$this.addClass('active');
+				$this.addClass("active");
 			}
 			e.preventDefault();
 
@@ -55,40 +55,40 @@ jQuery(document).ready(function ($) {
 				w = $this.width();
 
 			if (w > 768) {
-				if ($('body').hasClass('offcanvas-menu')) {
-					$('body').removeClass('offcanvas-menu');
+				if ($("body").hasClass("offcanvas-menu")) {
+					$("body").removeClass("offcanvas-menu");
 				}
 			}
-		})
+		});
 
-		$('body').on('click', '.js-menu-toggle', function (e) {
+		$("body").on("click", ".js-menu-toggle", function (e) {
 			var $this = $(this);
 			e.preventDefault();
 
-			if ($('body').hasClass('offcanvas-menu')) {
-				$('body').removeClass('offcanvas-menu');
-				$this.removeClass('active');
+			if ($("body").hasClass("offcanvas-menu")) {
+				$("body").removeClass("offcanvas-menu");
+				$this.removeClass("active");
 			} else {
-				$('body').addClass('offcanvas-menu');
-				$this.addClass('active');
+				$("body").addClass("offcanvas-menu");
+				$this.addClass("active");
 			}
-		})
+		});
 
 		// click outisde offcanvas
 		$(document).mouseup(function (e) {
 			var container = $(".site-mobile-menu");
 			if (!container.is(e.target) && container.has(e.target).length === 0) {
-				if ($('body').hasClass('offcanvas-menu')) {
-					$('body').removeClass('offcanvas-menu');
+				if ($("body").hasClass("offcanvas-menu")) {
+					$("body").removeClass("offcanvas-menu");
 				}
 			}
 		});
-	};
+	}
 	siteMenuClone();
 
 	function siteCarousel() {
-		if ($('.nonloop-block-13').length > 0) {
-			$('.nonloop-block-13').owlCarousel({
+		if ($(".nonloop-block-13").length > 0) {
+			$(".nonloop-block-13").owlCarousel({
 				center: false,
 				items: 1,
 				loop: true,
@@ -96,7 +96,7 @@ jQuery(document).ready(function ($) {
 				margin: 0,
 				autoplay: true,
 				nav: true,
-				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+				navText: ["<span class=\"icon-arrow_back\">", "<span class=\"icon-arrow_forward\">"],
 				responsive: {
 					600: {
 						margin: 0,
@@ -119,7 +119,7 @@ jQuery(document).ready(function ($) {
 			});
 		}
 
-		$('.slide-one-item').owlCarousel({
+		$(".slide-one-item").owlCarousel({
 			center: false,
 			items: 1,
 			loop: true,
@@ -128,10 +128,10 @@ jQuery(document).ready(function ($) {
 			autoplay: true,
 			pauseOnHover: false,
 			nav: true,
-			navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+			navText: ["<span class=\"icon-keyboard_arrow_left\">", "<span class=\"icon-keyboard_arrow_right\">"]
 		});
 
-		$('.slide-one-item-alt').owlCarousel({
+		$(".slide-one-item-alt").owlCarousel({
 			center: false,
 			items: 1,
 			loop: true,
@@ -146,16 +146,16 @@ jQuery(document).ready(function ($) {
 
 
 
-		$('.custom-next').click(function (e) {
+		$(".custom-next").click(function (e) {
 			e.preventDefault();
-			$('.slide-one-item-alt').trigger('next.owl.carousel');
+			$(".slide-one-item-alt").trigger("next.owl.carousel");
 		});
-		$('.custom-prev').click(function (e) {
+		$(".custom-prev").click(function (e) {
 			e.preventDefault();
-			$('.slide-one-item-alt').trigger('prev.owl.carousel');
+			$(".slide-one-item-alt").trigger("prev.owl.carousel");
 		});
 
-	};
+	}
 	siteCarousel();
 
 	function siteStellar() {
@@ -165,41 +165,41 @@ jQuery(document).ready(function ($) {
 			parallaxElements: true,
 			horizontalScrolling: false,
 			hideDistantElements: false,
-			scrollProperty: 'scroll'
+			scrollProperty: "scroll"
 		});
-	};
+	}
 	siteStellar();
 
 	function siteDatePicker() {
 
-		if ($('.datepicker').length > 0) {
-			$('.datepicker').datepicker();
+		if ($(".datepicker").length > 0) {
+			$(".datepicker").datepicker();
 		}
 
-	};
+	}
 	siteDatePicker();
 
 	function siteSticky() {
 		$(".js-sticky-header").sticky({ topSpacing: 0 });
-	};
+	}
 	siteSticky();
 
 	// navigation
 	function OnePageNavigation() {
-		var navToggler = $('.site-menu-toggle');
+		var navToggler = $(".site-menu-toggle");
 		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function (e) {
 			e.preventDefault();
 
 			var hash = this.hash;
 
-			$('html, body').animate({
-				'scrollTop': $(hash).offset().top
-			}, 600, 'easeInOutCirc', function () {
+			$("html, body").animate({
+				"scrollTop": $(hash).offset().top
+			}, 600, "easeInOutCirc", function () {
 				window.location.hash = hash;
 			});
 
 		});
-	};
+	}
 	OnePageNavigation();
 
 	function siteScroll() {
@@ -208,42 +208,42 @@ jQuery(document).ready(function ($) {
 			var st = $(this).scrollTop();
 
 			if (st > 100) {
-				$('.js-sticky-header').addClass('shrink');
+				$(".js-sticky-header").addClass("shrink");
 			} else {
-				$('.js-sticky-header').removeClass('shrink');
+				$(".js-sticky-header").removeClass("shrink");
 			}
 
-		})
+		});
 
-	};
+	}
 	siteScroll();
 
 });
 
 
 function submit_btn(e) {
-  // Prevent default behavior
-  // Prevents page from reloading and prevent the required check for the email
-  e.preventDefault();
+	// Prevent default behavior
+	// Prevents page from reloading and prevent the required check for the email
+	e.preventDefault();
 
-  const form = {
-    "fname": document.forms['contact_form'].elements["fname"].value,
-    "lname": document.forms['contact_form'].elements["lname"].value,
-    "email": document.forms['contact_form'].elements["email"].value,
-    "subject": document.forms['contact_form'].elements["subject"].value,
-    "message": document.forms['contact_form'].elements["message"].value,
-  };
-  console.log(form); // Debug
+	const form = {
+		"fname": document.forms["contact_form"].elements["fname"].value,
+		"lname": document.forms["contact_form"].elements["lname"].value,
+		"email": document.forms["contact_form"].elements["email"].value,
+		"subject": document.forms["contact_form"].elements["subject"].value,
+		"message": document.forms["contact_form"].elements["message"].value,
+	};
+	console.log(form); // Debug
 
-  // If neither name is given, or email is not given
-  if ((!form.fname && !form.lname) || !form.email) {
-    // Alert and ask user to redo it
-    alert("Please fill in missing items. At least fill in either your first or last name, and provide a email!");
-    return; // Break out of the function
-  }
+	// If neither name is given, or email is not given
+	if ((!form.fname && !form.lname) || !form.email) {
+		// Alert and ask user to redo it
+		alert("Please fill in missing items. At least fill in either your first or last name, and provide a email!");
+		return; // Break out of the function
+	}
 
-  /**
-   * @Todo Submit details to store in database
-   * @Todo Call email sending API
-   */
+	/**
+	 * @Todo Submit details to store in database
+	 * @Todo Call email sending API
+	 */
 }
